1a9f67034da4ded1ed8d6a68449d4c1908dfd613
Mark David Avery <mark@webark.cc>
2021-12-02 13:02:09 -0800

    fix: only have to test the ends of the range cause the other values are shared

--- solutions/2021/01/solution.js
+++ solutions/2021/01/solution.js
@@ -1,17 +1,8 @@
-function setSum(input, index, range) {
-  return input.slice(index, index + range).reduce((a, b) => parseInt(a) + parseInt(b), 0);
-}
-
 export default function solution(input, range = 1) {
-  let count = 0;
-
-  for (let i = 0; i < (input.length - range); ++i) {
-    if (setSum(input, i + 1, range) > setSum(input, i, range)) {
-      count++;
-    }
-  }
-
-  return count;
+  return input.reduce(function(count, depth, index, array) {
+    const isDepthIncreasing = parseInt(depth) < parseInt(array[index + range]);
+    return count + Number(isDepthIncreasing);
+  }, 0);
 }
 
 export function silver(input) {
