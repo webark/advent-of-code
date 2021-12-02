fd5adce2cd3771a1c3e265794d7264f573d60085
Mark David Avery <mark@webark.cc>
2021-12-02 01:34:13 -0800

    fix: updated the needless shifting of 1

--- solutions/2021/01/solution.js
+++ solutions/2021/01/solution.js
@@ -5,8 +5,8 @@ function setSum(input, index, range) {
 export default function solution(input, range = 1) {
   let count = 0;
 
-  for (let i = 1; i < (input.length - range + 1); i++) {
-    if (setSum(input, i, range) > setSum(input, i - 1, range)) {
+  for (let i = 0; i < (input.length - range); ++i) {
+    if (setSum(input, i + 1, range) > setSum(input, i, range)) {
       count++;
     }
   }
