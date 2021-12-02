d81e7d4d950cafcf0cfa229347eae8dca7fce3f2
Mark David Avery <mark@webark.cc>
2021-12-02 13:05:59 -0800

    fix: updated to silver and gold nomenclature

--- _template/test.js
+++ _template/test.js
@@ -4,11 +4,11 @@ import { strict as assert } from 'assert';
 import { silver, gold } from './solution.js'
 import { example } from './input.js'
 
-test('part 1 example', () => {
+test('silver example', () => {
   assert.equal(silver(example), 0);
 })
 
-test('part 2 example', () => {
+test('gold example', () => {
   assert.equal(gold(example), 0);
 })
 
