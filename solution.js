b43e7ba5475b1da41f381856075c1d3a9ef3030b
Mark David Avery <mark@webark.cc>
2021-12-03 11:26:26 -0800

    feat: added timing tracking

--- solution.js
+++ solution.js
@@ -1,4 +1,5 @@
 import colors from 'colors'
+import { performance } from 'perf_hooks';
 
 const year = process.env.YEAR
 const day = process.env.DAY
@@ -6,8 +7,16 @@ const day = process.env.DAY
 const { silver, gold } = await import(`./solutions/${year}/${day}/solution.js`);
 const { data } = await import(`./solutions/${year}/${day}/input.js`);
 
-const silverSolution = silver(data);
-const goldSolution = gold(data);
+function timedFunction(method) {
+  const start = performance.now();
+  return {
+    result: method(),
+    time: performance.now() - start,
+  };
+}
+
+const silverSolution = timedFunction(() => silver(data));
+const goldSolution = timedFunction(() => gold(data));
 
 console.log('                                       '.bgRed)
 
@@ -20,10 +29,12 @@ console.log('                                       '.bgRed)
 console.log('')
 
 console.log(`🎄🎄 DAY ${day} SILVER 🎄🎄`.bold.green)
-console.log(`${silverSolution}\n`)
+console.log(`${silverSolution.result} in ${silverSolution.time}`)
+
+console.log('')
 
 console.log(`🎄🎄 DAY ${day} GOLD 🎄🎄`.bold.green)
-console.log(goldSolution)
+console.log(`${goldSolution.result} in ${goldSolution.time}`)
 
 console.log('')
 
