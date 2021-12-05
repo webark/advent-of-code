ec7d051c5ce0075092fe3d0dab62829804c7dbdf
Mark David Avery <mark@webark.cc>
2021-12-04 22:39:19 -0800

    fix: refactored for clarity

--- solutions/2021/04/solution.js
+++ solutions/2021/04/solution.js
@@ -12,42 +12,64 @@
 
 //   return 0;
 // }
+function processInput(input) {
+  const [numbers, ...boards] = input;
+
+  return {
+    numbers: numbers.split(','),
+    boards: boards.map(board => board.split('\n').map(line => line.match(/\d+/g))),
+  };
+}
+
 
 function getRowColumn(board, number) {
-  const match = board.match(RegExp(`([^]*)${number.padStart(2, ' ')}\\b`));
-  if (match) {
-    const currentPlace = match[1].length / 3;
-    return [Math.floor(currentPlace / 5), currentPlace % 5];
+  for (const [rowIndex, row] of board.entries()) {
+    for (const [columnIndex, currentNumber] of row.entries()) {
+      if (number === currentNumber) {
+        return [rowIndex, columnIndex];
+      }
+    }
+  }
+
+  return [];
+}
+
+function getFoundKeys(board, number, boardNumber) {
+  const [rowIndex, columnIndex] = getRowColumn(board, number);
+
+  if (rowIndex !== undefined) {
+    const boardKey = `b${boardNumber}`;
+    return [`${boardKey}.r${rowIndex}`, `${boardKey}.c${columnIndex}`]
   }
 
   return [];
 }
 
 function getBoardTotal(board, number, runningTotoal = 0) {
-  return (board.match(/\d+/g).reduce((a, b) => parseInt(a) + parseInt(b)) - runningTotoal - number) * number
+  return (board.flat().reduce((a, b) => parseInt(a) + parseInt(b)) - runningTotoal - number) * number
 }
 
+
 export default function solution(input) {
-  const [numbers, ...boards] = input;
+  const { numbers, boards } = processInput(input);
   const found = new Map();
   const boardTotals = [];
 
-  for (const number of numbers.split(',')) {
+  for (const number of numbers) {
     for (const [boardNumber, board] of boards.entries()) {
-      const [row, column] = getRowColumn(board, number);
-      if (row === undefined) continue;
-      const boardKey = `b${boardNumber}`;
-      const rowKey = `${boardKey}.r${row}`;
-      const columnKey = `${boardKey}.c${column}`;
-      const foundRow = found.get(rowKey) || [];
-      const foundColumn = found.get(columnKey) || [];
+      const [rowKey, columnKey] = getFoundKeys(board, number, boardNumber);
 
-      if (foundRow.length === 4 || foundColumn.length === 4) {
+      if (rowKey === undefined) continue;
+
+      const foundRow = (found.get(rowKey) || []).concat(number);
+      const foundColumn = (found.get(columnKey) || []).concat(number);
+      found.set(rowKey, foundRow);
+      found.set(columnKey, foundColumn);
+
+      if (foundRow.length === 5 || foundColumn.length === 5) {
         return getBoardTotal(board, number, boardTotals[boardNumber]);;
       } else {
         boardTotals[boardNumber] = parseInt(number) + (boardTotals[boardNumber] || 0)
-        found.set(rowKey, foundRow.concat(number));
-        found.set(columnKey, foundColumn.concat(number));
       }
     }
   }
@@ -56,19 +78,16 @@ export default function solution(input) {
 }
 
 export function solution2(input) {
-  const [numbers, ...boards] = input;
+  const { numbers, boards } = processInput(input);
   const found = new Map();
   const matchedBoards = [];
   const boardTotals = [];
 
-  for (const number of numbers.split(',')) {
+  for (const number of numbers) {
     for (const [boardNumber, board] of boards.entries()) {
       if (matchedBoards.includes(boardNumber)) continue;
-      const [row, column] = getRowColumn(board, number);
-      if (row === undefined) continue;
-      const boardKey = `b${boardNumber}`;
-      const rowKey = `${boardKey}.r${row}`;
-      const columnKey = `${boardKey}.c${column}`;
+      const [rowKey, columnKey] = getFoundKeys(board, number, boardNumber);
+      if (rowKey === undefined) continue;
       const foundRow = found.get(rowKey) || [];
       const foundColumn = found.get(columnKey) || [];
 
