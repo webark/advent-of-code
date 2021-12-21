8ff1ffbc6c1bbf2278f3ef75fa28afc984986426
Mark David Avery <mark@webark.cc>
2021-12-20 16:42:28 -0800

    feat: frist pass of day 18

---
+++ solutions/2021/18/solution.js
@@ -0,0 +1,213 @@
+class Fish {
+  number = 0;
+  depth = 0;
+  position = 0;
+  next = null;
+  prev = null;
+
+  constructor(node) {
+    Object.assign(this, node);
+  }
+
+  explode() {
+    const { number, depth, position } = this.out;
+
+    this.prev.number += number;
+
+    if (this.next.next) this.next.next.number += this.next.number;
+
+    const fish = new Fish({
+      number: 0,
+      depth: depth - 1,
+      position: Number(this.prev.position === position),
+      prev: this.prev,
+      next: this.next.next,
+    })
+
+    fish.prev.next = fish;
+    if (fish.next) fish.next.prev = fish;
+
+    return fish;
+  }
+
+  split() {
+    const { number, depth } = this.out;
+
+    const rightFish = new Fish({
+      number: Math.floor(number / 2),
+      depth: depth + 1,
+      position: 0,
+      prev: this.prev,
+    });
+
+    const leftFish = new Fish({
+      number: Math.ceil(number / 2),
+      depth: depth + 1,
+      position: 1,
+      prev: rightFish,
+      next: this.next,
+    });
+
+    rightFish.prev.next = rightFish
+    rightFish.next = leftFish;
+    if (leftFish.next) leftFish.next.prev = leftFish
+
+    return leftFish;
+  }
+
+  get out() {
+    return {
+      number: this.number,
+      position: this.position,
+      depth: this.depth,
+    }
+  }
+}
+
+function parseFish(line, preFish) {
+  let depth = -1;
+  let position = 0;
+  let currentFish = preFish;
+
+  for (const char of line) {
+    switch (char) {
+      case '[':
+        depth++;
+        position = 0;
+        break;
+      case ']':
+        depth--;
+        position = 0;
+        break;
+      case ',':
+        position = Number(!position);
+        break;
+      default:
+        const nextFish = new Fish({
+          number: parseInt(char),
+          depth,
+          position,
+          prev: currentFish,
+        });
+
+        if (currentFish) {
+          currentFish.next = nextFish;
+        }
+
+        currentFish = currentFish.next;
+    }
+  }
+
+  return currentFish;
+}
+
+
+function raiseDepth(currentFish) {
+  while(currentFish.next) {
+    currentFish = currentFish.next;
+    currentFish.depth++;
+  }
+}
+
+function explodeAllFish(preFish) {
+  let currentFish = preFish;
+
+  while(currentFish.next) {
+    currentFish = currentFish.next;
+
+    if (currentFish.depth > 3) {
+      currentFish = currentFish.explode();
+    } else {
+      continue;
+    }
+
+    return explodeAllFish(preFish);
+  }
+
+  return splitAllFish(preFish);
+}
+
+function splitAllFish(preFish) {
+  let currentFish = preFish;
+
+  while(currentFish.next) {
+    currentFish = currentFish.next;
+
+    if (currentFish.number > 9) {
+      currentFish = currentFish.split();
+    } else {
+      continue;
+    }
+
+    return explodeAllFish(preFish);
+  }
+
+  return currentFish;
+}
+
+
+export function solution(input) {
+  const preFish = { reason: 'start' };
+  let lastFish = preFish;
+
+  for (const [index, line] of input.entries()) {
+    lastFish = parseFish(line, lastFish);
+    if (index) {
+      raiseDepth(preFish);
+      lastFish = explodeAllFish(preFish);
+    }
+  }
+
+  getMagnitude(preFish);
+
+  return preFish.next.number;
+}
+
+function getMagnitude(preFish, currentDepth = 2) {
+  let currentFish = preFish;
+
+  while(currentFish.next) {
+    currentFish = currentFish.next;
+    if (currentFish.depth > currentDepth) {
+      const magA = currentFish.number * (3 - currentFish.position);
+      const magB = currentFish.next.number * (3 - currentFish.next.position);
+      const newFish = new Fish({
+        number: magA + magB,
+        depth: currentFish.depth - 1,
+        position: Number(currentFish.prev.position === currentFish.position),
+        prev: currentFish.prev,
+        next: currentFish.next.next,
+        reason: 'summed',
+      })
+
+      newFish.prev.next = newFish;
+      if (newFish.next) newFish.next.prev = newFish;
+
+    } else {
+      continue;
+    }
+
+    return getMagnitude(preFish, currentDepth)
+  }
+
+  if (currentDepth >= 0) {
+    return getMagnitude(preFish, --currentDepth)
+  }
+}
+
+function logFish(currentFish) {
+  console.log()
+
+  while(currentFish.next) {
+    currentFish = currentFish.next;
+    console.log(currentFish.out);
+  }
+}
+
+export function silver(input) {
+  return solution(input);
+}
+
+export function gold(input) {
+  return 0;
+}
