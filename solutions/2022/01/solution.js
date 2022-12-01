9544b1bc86fd8d78d34baa263aa3e5cd40b1f715
Mark David Avery <mark@webark.cc>
2022-12-01 05:57:53 -0800

    feat: day 1 solution for 2022

---
+++ solutions/2022/01/solution.js
@@ -0,0 +1,19 @@
+export function solution(input, amount) {
+  return sum(numSort(input.map(sum)).slice(-amount));
+}
+
+export function silver(input) {
+  return solution(input, 1);
+}
+
+export function gold(input) {
+  return solution(input, 3);
+}
+
+function sum(array) {
+  return array.reduce((sum, value) => sum + value);
+}
+
+function numSort(array) {
+  return array.sort((a,b) => a - b);
+}
